1707de4761a32af2be0da130efe817de99639f49
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-02-17 22:40:40 +0530

    Printing Initial Position of the player

---
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package com.snakeandladder;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		// Initial position is 0
+		final int playerposition = 0;
+		System.out.println("Welcome to Snake And Ladder program");
+		System.out.println("Player at Start position " + playerposition);
+	}
+
+}
